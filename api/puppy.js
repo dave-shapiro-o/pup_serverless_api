782c7d18200e39aaa38c771d9aaa8d9d49fe97d0
Dave Shapiro <dave-shapiro-o>
2022-12-14 23:36:09 +0000

    working on localhost (no cors) -  retrieves first vid url

--- api/puppy.js
+++ api/puppy.js
@@ -1,26 +1,28 @@
 module.exports = (req, res) => {
     if(req.method === 'GET'){
-        const resultsUrl = req.query.url;
   const puppeteer = require("puppeteer");
 
   async function getVideoUrl() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(resultsUrl, { timeout: 0, waitUntil: "networkidle0" });
+    const query = req.query.url;
 
-    const title = await page.title();
-    const [url] = await page.evaluate(() => 
-      Array.from(document.querySelectorAll('a[href*="www.youtube.com/watch?v="]'), element => element.href));
+    await page.goto(`https://www.google.com/search?q=${query}&biw=1410&bih=715&tbm=vid`, 
+    { timeout: 0, waitUntil: "networkidle0" });
 
-    return url ? url : `No useful results found in search: ${title}`;
+    // const content = await (await page.$$eval('a', (El) => El.map((el) => el.getAttribute('href'))))
+    // ;
+    const content = await page.$$eval(".LC20lb", els => 
+      els.map(e => ({title: e.innerText, link: e.parentNode.href}))
+    );
+    const link = content[0].link
+    // const [url] = await page.evaluate(() => 
+    //     Array.from(document.querySelectorAll('a[href*="www.youtube.com/watch?v="]'), element => element.href));
+    return link ? link : `No useful results found in search: ${title}`;
   }
   getVideoUrl().then((videoUrl) => {
     res.send(`{"videoUrl": "${videoUrl}"}`);
   });
-
-        // res.json([
-        //     {url: "https://www.youtube.com/watch?v=BhArBPtW6Ms"}
-        // ])
     }else {
         res.send("Das is verboten!!")
     }
