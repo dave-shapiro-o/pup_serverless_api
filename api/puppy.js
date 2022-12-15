650afd7ec09aad1aa6f23738341de1eb3af2a4cd
Dave Shapiro <dave-shapiro-o>
2022-12-15 03:09:57 +0000

    cors working locally again

--- api/puppy.js
+++ api/puppy.js
@@ -1,19 +1,38 @@
-module.exports = (req, res) => {
+const allowCors = fn => async (req, res) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+  }
+
+const handler = (req, res) => {
     if(req.method === 'GET'){
   const puppeteer = require("puppeteer");
 
   async function getVideoUrl() {
-    const browser = await puppeteer.launch()
-    
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     const query = req.query.url;
 
     await page.goto(`https://www.google.com/search?q=${query}&biw=1410&bih=715&tbm=vid`, 
     { timeout: 0, waitUntil: "networkidle0" });
 
-    // const content = await (await page.$$eval('a', (El) => El.map((el) => el.getAttribute('href'))));
+    // const content = await (await page.$$eval('a', (El) => El.map((el) => el.getAttribute('href'))))
+    // ;
     const content = await page.$$eval(".LC20lb", els => 
-      els.map(e => ({title: e.innerText, link: e.parentNode.href}))
+      els
+      .map(e => ({title: e.innerText, link: e.parentNode.href}))
       .filter(e => e.link.includes ('www.youtube.com/watch?v='))
     );
     const link = content[0].link
@@ -27,6 +46,7 @@ module.exports = (req, res) => {
     }else {
         res.send("Das is verboten!!")
     }
-
+    
     if(req.method === 'OPTIONS') { return res.status(200).json(({ body: "OK" })) }
 }
+module.exports = allowCors(handler)
